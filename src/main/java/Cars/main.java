733533837f613bd00ce6a01642a4d33753c321bc
Billy Barker <BBarker@softwareinstitue.com>
2021-09-24 15:08:28 +0100

    I dont enjoy this project

--- src/main/java/Cars/main.java
+++ src/main/java/Cars/main.java
@@ -2,16 +2,16 @@ package Cars;
 
 public class main {
     public static void main(String[] args)  {
-        NumberPlateFinder hello = new NumberPlateFinder();
+        NumberPlateFinder i = new NumberPlateFinder();
         seleiniumChecker j = new seleiniumChecker();
 
         try {
-            hello.stress();
+            i.stress();
         } catch (Exception e) {
             e.printStackTrace();
         }
         j.setUp();
-        j.iAmSoAngry(hello.getNumbers().get(0));
+        j.iAmSoAngry(i.getNumbers().get(0));
         j.tearDown();
 
     }
